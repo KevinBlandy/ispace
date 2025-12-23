8c683774621207be4e3d4a12e091836470db2972
Yee <kevinblandy.cn@gmail.com>
2025-12-23 18:30:32 +0800

    update

--- repo/model/model.go
+++ repo/model/model.go
@@ -1,31 +1,5 @@
 package model
 
-type UserRole string
-
-var (
-	// UserRoleAdmin 管理员
-	UserRoleAdmin UserRole = "admin"
-	// UserRoleMember 会员
-	UserRoleMember UserRole = "member"
-)
-
-// User 管理员
-type User struct {
-	Id         int64    `gorm:"primaryKey;autoIncrement"`
-	Avatar     string   // 头像
-	Account    string   // 账户
-	Password   string   // 密码
-	Email      string   // 邮箱
-	Role       UserRole // 角色
-	Enabled    bool     // 是否启用账户
-	CreateTime int64    // 创建时间
-	UpdateTime int64    // 更新时间
-}
-
-func (User) TableName() string {
-	return "t_user"
-}
-
 type ObjectCompression string
 
 var (
@@ -52,29 +26,4 @@ type Object struct {
 
 func (Object) TableName() string {
 	return "t_object"
-}
-
-// UserObject 会员资源
-type UserObject struct {
-	Id         int64  `gorm:"primaryKey;autoIncrement"` // 资源 ID
-	UserId     int64  // 会员 ID
-	ObjectId   int64  // 物理资源 ID，如果是目录则为 0
-	ParentId   int64  // 父级ID
-	Title      string // 资源标题
-	Dir        bool   // 是否是目录
-	Path       string // ID 关系树
-	Depth      uint64 // 树深度
-	DeleteTime int64  // 删除时间
-	CreateTime int64  // 创建时间
-	UpdateTime int64  // 更新时间
-}
-
-func (UserObject) TableName() string {
-	return "t_user_object"
-}
-
-type RecycleBin struct {
-	Id           int64 `gorm:"primaryKey;autoIncrement"`
-	UserObjectId int64 // 会员资源 ID
-	CreateTime   int64 // 创建时间
 }
